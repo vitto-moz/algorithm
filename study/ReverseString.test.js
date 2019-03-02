6ddc97a2a8ac2e5b605140b40da273d8a1efd0a1
vitto <VMozharovsky@smartspaceplc.com>
2019-03-02 20:56:40 +0200

    stydy folder added

---
+++ study/ReverseString.test.js
@@ -0,0 +1,24 @@
+const {assert} = require('chai');
+
+function stringReversal(string) {
+    return string.split('').reduce((gathered, letter) => {
+        return letter + gathered
+    })
+}
+
+function _stringReversal(string) {
+    let reversedString = ''
+    for (letter of string) {
+        reversedString = letter + reversedString
+    }
+    return reversedString
+}
+
+
+
+describe.only("String Reversal", () => {
+    it("Should retrun string reverse", () => {
+        assert.equal(stringReversal("Hello World!"), "!dlroW olleH");
+        assert.equal(_stringReversal("Hello World!"), "!dlroW olleH");
+    });
+});
